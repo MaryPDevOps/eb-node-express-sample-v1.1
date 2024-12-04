35d1a52fb9a0f1d7dd32b8c1973b960a165ac63c
MaryPDevOps <mary.pedasingu@gmail.com>
2024-12-04 16:58:55 +0530

    my first commit

--- AspNetAppWithWhiteSourceAndSonarNew/AspNetAppWithWhiteSourceAndSonarNew/Properties/AssemblyInfo.cs
+++ AspNetAppWithWhiteSourceAndSonarNew/AspNetAppWithWhiteSourceAndSonarNew/Properties/AssemblyInfo.cs
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("f67c0a36-9261-4f8b-8a52-12bb44ede12a")]
+[assembly: Guid("b4fd844e-2fb8-44ca-afa7-75f720a65e16")]
 
 // Version information for an assembly consists of the following four values:
 //
